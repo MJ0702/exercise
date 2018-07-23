37d2a701cd56899f03932c51fd5ac2ffc91d67d9
MJ0702 <781082627@qq.com>
2018-07-23 16:53:58 +0800

    form

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,22 @@
 <template>
+  <!-- 按钮显隐-->
   <div>
     <el-button @click="show2 = !show2">Click Me</el-button>
 
     <div style="display: flex; margin-top: 20px; height: 100px;">
       <transition name="el-zoom-in-center">
-        <div v-show="show2" class="transition-box">中</div>
+        <div v-show="show2" class="transition-box">1</div>
       </transition>
 
       <transition name="el-zoom-in-top">
-        <div v-show="show2" class="transition-box">上</div>
+        <div v-show="show2" class="transition-box">2</div>
       </transition>
 
       <transition name="el-zoom-in-bottom">
-        <div v-show="show2" class="transition-box">下</div>
+        <div v-show="show2" class="transition-box">3</div>
       </transition>
     </div>
+    <!-- 日期选择 -->
     <div class="date_picker">
       <el-date-picker
         v-model="date"
@@ -26,23 +28,139 @@
         :default-time="['00:00:00', '23:59:59']">
       </el-date-picker>
     </div>
+    <!--表单验证-->
+    <div class="form_rules">
+      <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="密码" prop="pass">
+          <el-input type="password" v-model="ruleForm2.pass" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="checkPass">
+          <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="年龄" prop="age">
+          <el-input v-model.number="ruleForm2.age"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+          <el-button @click="resetForm('ruleForm2')">重置</el-button>
+        </el-form-item>
+      </el-form>
+      <el-form :model="numberValidateForm" ref="numberValidateForm" label-width="100px" class="demo-ruleForm">
+        <el-form-item
+          label="年龄"
+          prop="age"
+          :rules="[
+            { required: true, message: '年龄不能为空'},
+            { type: 'number', message: '年龄必须为数字值'}
+          ]"
+        >
+          <el-input type="age" v-model.number="numberValidateForm.age" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('numberValidateForm')">提交</el-button>
+          <el-button @click="resetForm('numberValidateForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
+
 <script>
-    export default {
-    data: () => ({
-      show2: true,
-      date: []
-    }),
-     methods:{
-        timeChange(val) {
-          console.log(val)
+  export default {
+    data() {
+      var checkAge = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('年龄不能为空'));
         }
-     }
+        setTimeout(() => {
+          if (!Number.isInteger(value)) {
+            callback(new Error('请输入数字值'));
+          } else {
+            if (value < 18) {
+              callback(new Error('必须年满18岁'));
+            } else {
+              callback();
+            }
+          }
+        }, 1000);
+      };
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.ruleForm2.checkPass !== '') {
+            this.$refs.ruleForm2.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.ruleForm2.pass) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
+      return {
+        show2: true,
+        date: [],
+        numberValidateForm: {
+          age: ''
+        },
+        ruleForm2: {
+          pass: '',
+          checkPass: '',
+          age: ''
+        },
+        rules2: {
+          pass: [
+            { validator: validatePass, trigger: 'blur' }
+          ],
+          checkPass: [
+            { validator: validatePass2, trigger: 'blur' }
+          ],
+          age: [
+            { validator: checkAge, trigger: 'blur' }
+          ]
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      },
+      timeChange(val) {
+          console.log(val)
+        },
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            // console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
   }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 
 <style>
@@ -58,7 +176,10 @@
     box-sizing: border-box;
     margin-right: 20px;
   }
-  .date_picker{
+  .date_picker,.form_rules{
     margin-top: 20px;
+  }
+  .form_rules{
+    width: 500px;
   }
 </style>
