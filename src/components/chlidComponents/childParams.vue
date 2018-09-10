9adbd53b08b490fb0bc4e66dc32399ac5122ef2d
MJ0702 <781082627@qq.com>
2018-09-10 17:38:30 +0800

    child

---
+++ src/components/chlidComponents/childParams.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>接受父组件的值</div>
+    <p>{{params}}</p>
+    <div>接受父组件的方法</div>
+    <div>
+    <el-row>
+        <el-button type="primary" plain @click="child_show">父组件方法(子组件)</el-button>
+    </el-row>
+    </div>
+  </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+            }
+        },
+        props:['params'],
+        methods:{
+            child_show(){
+                this.$emit('fun','666');
+            }
+        }
+    }
+</script>
+<style lang="less">
+@width:200px;
+@height:100px;
+    // p{
+    //     width:@width;
+    //     height:@height;
+    // }
+</style>
--- src/components/product.vue
+++ src/components/product.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
     <h1>product</h1>
-    <p>{{productMsg}}</p>
+    <el-row>
+        <el-button type="info" plain @click="show(888)">父组件方法(父组件)</el-button>
+    </el-row>
+    <childParams v-bind:params="msg" @fun="show"></childParams>
   </div>
 </template>
 <script>
+   import childParams from '@/components/chlidComponents/childParams'
     export default {
         data () {
             return {
-                productMsg: '我是product组件'
+                msg: '我是product组件(父组件的值)'
+            }
+        },
+        components:{
+            childParams:childParams
+        },
+        methods:{
+            show(data){
+                console.log('这是父组件的方法!'+data);
             }
         }
     }
