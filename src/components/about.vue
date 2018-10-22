732375ed327d0543df883599a6b2e46efb63f823
MJ0702 <781082627@qq.com>
2018-10-22 16:32:43 +0800

    图片懒加载

--- src/components/about.vue
+++ src/components/about.vue
@@ -2,13 +2,74 @@
   <div>
     <h1>about</h1>
     <p>{{aboutMsg}}</p>
+    <ul id="container">
+        <li v-for="item in imgList" :key="item.id">
+            <img v-lazy="item.url">
+        </li>
+    </ul>
   </div>   
 </template>
 <script>
     export default {
         data () {
             return {
-                aboutMsg: '我是about组件'
+                aboutMsg: 'vue-lazyload 图片懒加载',
+                imgList:[
+                    {id:0,url:require('../assets/1.jpg')},
+                    {id:1,url:require('../assets/3.jpg')},
+                    {id:2,url:require('../assets/4.jpg')},
+                    {id:3,url:require('../assets/5.jpg')},
+                    {id:4,url:require('../assets/6.jpg')},
+                    {id:5,url:require('../assets/7.jpg')},
+                    {id:6,url:require('../assets/1.jpg')},
+                    {id:7,url:require('../assets/3.jpg')},
+                    {id:8,url:require('../assets/4.jpg')},
+                    {id:9,url:require('../assets/5.jpg')},
+                    {id:10,url:require('../assets/6.jpg')},
+                    {id:11,url:require('../assets/7.jpg')},
+                    {id:12,url:require('../assets/1.jpg')},
+                    {id:13,url:require('../assets/3.jpg')},
+                    {id:14,url:require('../assets/4.jpg')},
+                    {id:15,url:require('../assets/5.jpg')},
+                    {id:16,url:require('../assets/6.jpg')},
+                    {id:17,url:require('../assets/7.jpg')},
+                    {id:18,url:require('../assets/1.jpg')},
+                    {id:19,url:require('../assets/3.jpg')},
+                    {id:20,url:require('../assets/4.jpg')},
+                    {id:21,url:require('../assets/5.jpg')},
+                    {id:22,url:require('../assets/6.jpg')},
+                    {id:23,url:require('../assets/7.jpg')},
+                    {id:24,url:require('../assets/1.jpg')},
+                    {id:25,url:require('../assets/3.jpg')},
+                    {id:26,url:require('../assets/4.jpg')},
+                    {id:27,url:require('../assets/5.jpg')},
+                    {id:28,url:require('../assets/6.jpg')},
+                    {id:29,url:require('../assets/7.jpg')},
+                    {id:30,url:require('../assets/1.jpg')},
+                    {id:31,url:require('../assets/3.jpg')},
+                    {id:32,url:require('../assets/4.jpg')},
+                    {id:33,url:require('../assets/5.jpg')},
+                    {id:34,url:require('../assets/6.jpg')},
+                    {id:35,url:require('../assets/7.jpg')},
+                    {id:36,url:require('../assets/1.jpg')},
+                    {id:37,url:require('../assets/3.jpg')},
+                    {id:38,url:require('../assets/4.jpg')},
+                    {id:39,url:require('../assets/5.jpg')},
+                    {id:40,url:require('../assets/6.jpg')},
+                    {id:41,url:require('../assets/7.jpg')},
+                    {id:42,url:require('../assets/1.jpg')},
+                    {id:43,url:require('../assets/3.jpg')},
+                    {id:44,url:require('../assets/4.jpg')},
+                    {id:45,url:require('../assets/5.jpg')},
+                    {id:46,url:require('../assets/6.jpg')},
+                    {id:47,url:require('../assets/7.jpg')},
+                    {id:48,url:require('../assets/1.jpg')},
+                    {id:49,url:require('../assets/3.jpg')},
+                    {id:50,url:require('../assets/4.jpg')},
+                    {id:51,url:require('../assets/5.jpg')},
+                    {id:52,url:require('../assets/6.jpg')},
+                    {id:53,url:require('../assets/7.jpg')}
+                ],
             }
         }
     }
@@ -16,8 +77,19 @@
 <style lang="less">
 @width:200px;
 @height:100px;
-    // p{
-    //     width:@width;
-    //     height:@height;
-    // }
+    #container{
+        li{
+            list-style: none;
+            img{
+                width: @width;
+                height: @height;
+                margin-bottom: 20px;
+            }
+            img[lazy=loading]{
+                width: 30px;
+                height: 30px;
+                margin-top: 50px;
+            }
+        }
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VueLazyload from 'vue-lazyload'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+//图片懒加载
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error:'./static/loaderror.png',
+  loading: './static/loading.gif',
+  attempt: 1,
+  throttleWait:300,  //等待秒数
+  listenEvents:['mousewheel','scroll ']  //监听事件
+})
 
 /* eslint-disable no-new */
 new Vue({
